91d889e5b19288c57e8b376d14a7d76c924319af
Martin Gontšarov <qwertyuk01@gmail.com>
2019-09-20 16:29:34 +0300

    Fixes and more dev

--- src/components/layout/Home.js
+++ src/components/layout/Home.js
@@ -1,24 +1,82 @@
 import React, { Component } from 'react'
 import './Home.css'
-import Logo from "../../images/WW_LOGO_TRNSPRNT.png"
-
+import Logo from "../../images/WW_LOGO_TRNSPRNT.png";
+import ints from '../../images/WW_indrek.png';
+import data from '../../languages';
 export class Home extends Component {
+
+    
+
+
+
     render() {
+        console.log(data)
         return (
             <div>
-                <h2 className="anno">ANNO<br></br>2014</h2>
-            <img src={Logo} className="logo" alt=""/>
+
+
             <div className='logo-section'>
-                <div className="logo-line"></div>
-                <div className="logo-line-divider"></div>
-                <div className="logo-line"></div>
+
+                <div className="logo-section-contact">
+                    <h2 style={{fontVariant: "small-caps"}}>+372 6008109<br/>11.00-22.00<br/>info@wanawiipuri.ee</h2>
+                    <div className="logo-line-left"></div>
+                </div>
+
+                <div className="logo">
+                    <h2 className="anno">ANNO<br></br>2014</h2>
+                    <img src={Logo} className="logo" alt=""/>
+                </div>
+                
+                <div className="logo-section-languages">
+                    <div className="inline">
+                        <button>RUS</button>
+                        <h2>•</h2>
+                        <button>ENG</button>
+                        <h2>•</h2>
+                        <button>EST</button>
+                        <h2>•</h2>
+                        <button>FIN</button>
+                        </div>
+                    <div className="logo-line-right"></div>
+                </div>
+               
+                {/* <div className="logo-line-divider"></div> */}
+                
+                
+                {/*  */}
+                {/* <div className="logo-line"></div> */}
             </div>
 
 
                 {/* <p>Contact</p> */}
                 <h1>Wana Wiipuri</h1>
+
+                <div className="container-fluid">
+
+                <div className="row">
+                {/* LEFT SIDE OF MENU */}
+                <div className="col-4">
+                    <div className="menu-entry">
+                        <h2 className="item-name"></h2>
+                        <h2 className="item-price"></h2>
+                        <p className="item-desc"></p>
+                    </div>
+
+
+                </div>
+                {/* PAPA PITCTURE */}
+                <div className="col-4">
+                    <img src={ints} className="ints" alt="Professor" />
+                </div>
+                {/* RIGHT SIDE OF MENU */}
+                <div className="col-4">
+
+                </div>
+
+                </div>
+                </div>
                 
-            </div>
+                </div>
         )
     }
 }
---
+++ src/languages.js
@@ -0,0 +1,13 @@
+const data = {
+    pickles: {
+        price: "5,0",
+        ru: {name: "Солёные огурчики Вальсберга", desc: "Малосольные огурцы / черемша / сметана / медово-горчичный соус"},
+        et: {name: "Walsberg'i soolakurgid", desc: "Marineeritud karulauk / hapukoor / mee-sinepikaste"},
+        fi: {name: "Walsberg'in suolakurkku", desc: "Karhunlaukkaa / smetanaa / hunaja-sinappikastiketta"},
+        en: {name: "Walsberg's salted pickles", desc: "Ramsons / sour cream / honey-mustard sauce"},
+    },
+
+}
+
+export default data
+
