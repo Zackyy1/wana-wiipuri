41f395069be3c2c91556c9dbf0d0bf6817e1e781
Martin Gontšarov <qwertyuk01@gmail.com>
2019-10-30 00:04:34 +0200

    more wine work

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,6 +1,18 @@
 import React, { Component } from 'react'
 import './css/Footer.css'
 export class Footer extends Component {
+    // componentDidMount() {
+    //     const script = document.createElement("script");
+    //     script.src="https://www.jscache.com/wejs?wtype=cdsratingsonlywide&amp;uniq=336&amp;locationId=7038006&amp;lang=en_CA&amp;border=false&amp;shadow=false&amp;backgroundColor=white&amp;display_version=2";
+    //     // script.data-loadtrk;
+    //     script.onload="this.loadtrk=true";
+    //     script.async = true;
+    //     document.body.appendChild(script);
+
+    // }
+    
+    
+
     render() {
         return (
             <div className="footer-bg">
@@ -10,6 +22,22 @@ export class Footer extends Component {
                     as well as more restaurant info and additional contact info
 
                 */}
+
+                {/* TRIPADVISOR FAILED EMBED */}
+                
+                {/* <div id="TA_cdsratingsonlywide336" class="TA_cdsratingsonlywide">
+                    <ul id="Pxefwp" class="TA_links ZLFMmXzUv">
+                        <li id="8ewUBoS03RNn" class="zk6Z92uJt9">
+                            <a target="_blank" href="https://www.tripadvisor.ca/">
+                                <img src="https://www.tripadvisor.ca/img/cdsi/img2/branding/tripadvisor_logo_transp_340x80-18034-2.png" alt="TripAdvisor"/>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <script async src="https://www.tripadvisor.com/WidgetEmbed-cdsratingsonlynarrow?amp;locationId=17821239&border=true&uniq=350&lang=en_US&display_version=2" data-loadtrk onload="this.loadtrk=true"></script>
+                */}
+               
+
             </div>
         )
     }
--- src/components/layout/Wines.js
+++ src/components/layout/Wines.js
@@ -1,10 +1,87 @@
 import React, { Component } from 'react'
 import './css/Wines.css'
+import Cookies from 'universal-cookie';
+import data from '../../languages';
+import Ornament from '../../images/ornaments-vector-3.png';
+import { directive } from '@babel/types';
 export class Wines extends Component {
+
+    getArrayOfItems(path, type) {
+        var array = [];
+        var object1 = data[path][type];
+        Object.keys(object1).map(drink => {
+            array.push(object1[drink]);
+        });
+        return array;
+    }
+
+    makeDrinks(path, type, lang) {
+        var array = this.getArrayOfItems(path, type);
+        return array.map(wineEntry => {
+            return (
+                <tr key={wineEntry['key']} className="wine-entry">
+                    { wineEntry['ru'] ? 
+                    (<td className="wine-entry-name">{wineEntry[lang]}</td>) : 
+                    (<td className="wine-entry-name">{wineEntry['name']}, <span className="wine-entry-country">{wineEntry['country']}</span></td>) }
+                    { wineEntry['unit'] ? 
+                    <td className="wine-entry-units">{wineEntry['amount']+wineEntry['unit']} </td> :
+                    <td className="wine-entry-units helper">{data.text.drink_info['blank']}</td>}
+                    <td className="wine-entry-price">{wineEntry['price']}</td>
+                    {/* <p className="drink-entry-name">{drinkEntry[lang]}</p> */}
+                </tr>
+            )
+        })
+    }
+
     render() {
+        const cookies = new Cookies();
+        var lang = cookies.get("lang") || "en";
+
         return (
-            <div>
-                <h2 className="wines-title">WINE CARD COMING SOON</h2>
+            <div className="wine_page">
+                <div className="wine-headers">
+                <div className="row">
+                    
+                        <div className="col-4 wine-header wine-header-round">{data.text.red_wines[lang]}</div>
+                        <div className="col-4 wine-header-main"><h2 className="wine-header-main">{data.text.wines_header[lang]}</h2></div>
+
+                        <div className="col-4 wine-header wine-header-round">{data.text.white_wines[lang]}</div>
+
+                    </div>
+                </div>
+                <div className="row red-white-row">
+                    <div className="col-6 red-wines">
+                        {/* RED WINES */}
+                        <table className="red-wines-table">
+                            <tbody>
+                                <tr className="wine-entry"> 
+                                    {/* HELPERS */}
+                                    <td className="wine-entry-name helper">{data.text.drink_info['blank']}</td>
+                                    <td className="wine-entry-unit helper">{data.text.drink_info.size[lang]}</td>
+                                    <td className="wine-entry-price helper">{data.text.drink_info.price[lang]}</td>
+                                </tr>
+                                {/*  DRINK LIST */}
+                                { this.makeDrinks('drinks', 'red_wines', lang) }
+                            </tbody>
+                        </table>
+                    </div>
+                    <div className="col-6 white-wines">
+                        {/* WHITE WINES */}
+                        <div ><img className="art-placeholder" src={Ornament} alt=""/></div>
+                        <table className="white-wines-table">
+                            <tbody>
+                                <tr className="wine-entry"> 
+                                    {/* HELPERS */}
+                                    <td className="wine-entry-name helper">{data.text.drink_info['blank']}</td>
+                                    <td className="wine-entry-unit helper">{data.text.drink_info.size[lang]}</td>
+                                    <td className="wine-entry-price helper">{data.text.drink_info.price[lang]}</td>
+                                </tr>
+                                {/*  DRINK LIST */}
+                                { this.makeDrinks('drinks', 'white_wines', lang) }
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
             </div>
         )
     }
