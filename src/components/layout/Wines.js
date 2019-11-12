29b65564200ac236af67258518bec88fe43521a5
Martin Gontšarov <qwertyuk01@gmail.com>
2019-11-12 15:38:24 +0200

    finished menu

--- src/components/layout/Wines.js
+++ src/components/layout/Wines.js
@@ -22,6 +22,24 @@ export class Wines extends Component {
                 <tr key={wineEntry['key']} className="wine-entry">
                     { wineEntry['ru'] ? 
                     (<td className="wine-entry-name">{wineEntry[lang]}</td>) : 
+                    (<td className="wine-entry-name">{wineEntry['name']} <span className="wine-entry-country">{wineEntry['country']}</span></td>) }
+                    { wineEntry['unit'] ? 
+                    <td className="wine-entry-units">{wineEntry['amount']+wineEntry['unit']} </td> :
+                    <td className="wine-entry-units helper">{data.text.drink_info['blank']}</td>}
+                    <td className="wine-entry-price">{wineEntry['price']}</td>
+                    {/* <p className="drink-entry-name">{drinkEntry[lang]}</p> */}
+                </tr>
+            )
+        })
+    }
+
+    makeWineDrinks(path, type, lang) {
+        var array = this.getArrayOfItems(path, type);
+        return array.map(wineEntry => {
+            return (
+                <tr key={wineEntry['key']} className="wine-entry">
+                    { wineEntry['ru'] ? 
+                    (<td className="wine-entry-name">{wineEntry[lang]}</td>) : 
                     (<td className="wine-entry-name">{wineEntry['name']}, <span className="wine-entry-country">{wineEntry['country']}</span></td>) }
                     { wineEntry['unit'] ? 
                     <td className="wine-entry-units">{wineEntry['amount']+wineEntry['unit']} </td> :
@@ -29,6 +47,21 @@ export class Wines extends Component {
                     <td className="wine-entry-price">{wineEntry['price']}</td>
                     {/* <p className="drink-entry-name">{drinkEntry[lang]}</p> */}
                 </tr>
+            )
+        })
+    }
+
+    makeDrinksList(path, type, lang) {
+        var array = this.getArrayOfItems(path, type);
+        return array.map(champagneEntry => {
+            return (
+                <div key={champagneEntry['key']} className="champagne-entry">
+                    <p className="champagne-entry-info">
+                        {champagneEntry['name']}
+                        <br></br>
+                        {champagneEntry['amount']}{champagneEntry['unit']} - {champagneEntry['price']}€
+                    </p>
+                </div>
             )
         })
     }
@@ -50,6 +83,7 @@ export class Wines extends Component {
                     </div>
                 </div>
                 <div className="row red-white-row">
+
                     <div className="col-6 red-wines">
                         {/* RED WINES */}
                         <table className="red-wines-table">
@@ -61,7 +95,7 @@ export class Wines extends Component {
                                     <td className="wine-entry-price helper">{data.text.drink_info.price[lang]}</td>
                                 </tr>
                                 {/*  DRINK LIST */}
-                                { this.makeDrinks('drinks', 'red_wines', lang) }
+                                { this.makeWineDrinks('drinks', 'red_wines', lang) }
                             </tbody>
                         </table>
                     </div>
@@ -77,11 +111,124 @@ export class Wines extends Component {
                                     <td className="wine-entry-price helper">{data.text.drink_info.price[lang]}</td>
                                 </tr>
                                 {/*  DRINK LIST */}
-                                { this.makeDrinks('drinks', 'white_wines', lang) }
+                                { this.makeWineDrinks('drinks', 'white_wines', lang) }
                             </tbody>
                         </table>
                     </div>
                 </div>
+                
+                <div className="row">
+                    <div className="col-8">
+                        {/* House Wines */}
+                        <div className="house-wines-round">
+                            <div className="house-white house-entry">
+                                <h2 className="house-wine-title">white wine</h2>
+                                <p className="house-wines-prices">75cl - 27€<br></br>
+                                24cl - 8,7€<br></br>
+                                16cl - 5,9€</p>
+                            </div>
+                            <div className="house-red house-entry">
+                                <h2 className="house-wine-title">red wine</h2>
+                                <p className="house-wines-prices">75cl - 27€<br></br>
+                                24cl - 8,7€<br></br>
+                                16cl - 5,9€</p>
+                            </div>
+                            <div className="house-rose house-entry">
+                                <h2 className="house-wine-title">rose wine</h2>
+                                <p className="house-wines-prices">75cl - 27€<br></br>
+                                24cl - 8,7€<br></br>
+                                16cl - 5,9€</p>
+                            </div>
+
+                            
+                        </div>
+
+                        {/* Grappa, vermut, dessert wines */}
+
+                        <div className="other-drinks">
+                            <div className="grappa-section">
+                                <h2 className="grappa-title">{data.text.grappa[lang]}</h2>
+                                <div ><img className="art-placeholder-2" src={Ornament} alt=""/></div>
+                                <table className="grappa-table">
+                                    <tbody>
+                                        <tr className="wine-entry"> 
+                                            {/* HELPERS */}
+                                            <td className="wine-entry-name helper">{data.text.drink_info['blank']}</td>
+                                            <td className="wine-entry-unit helper">{data.text.drink_info.size[lang]}</td>
+                                            <td className="wine-entry-price helper">{data.text.drink_info.price[lang]}</td>
+                                        </tr>
+                                        {/*  DRINK LIST */}
+                                        { this.makeDrinks('drinks', 'grappa', lang) }
+                                    </tbody>
+                                </table>
+                            </div>
+
+
+                            <div className="row other-section">
+                                {/*  VERMUT AND DESSERT */}
+                                <div className="col-6">
+                                    {/* Vermut */}
+                                    <h2 className="vermut-title">
+                                        {data.text.vermut[lang]}
+                                        <span className="and"> & </span>
+                                        {data.text.vermut2[lang]}
+                                    </h2>
+
+                                    <div className="vermut-list">
+                                        <table className="vermut-table">
+                                            <tbody>
+                                                <tr className="other-drink-entry"> 
+                                                    {/* HELPERS */}
+                                                    <td className="wine-entry-name helper">{data.text.drink_info['blank']}</td>
+                                                    <td className="wine-entry-unit helper">{data.text.drink_info.size[lang]}</td>
+                                                    <td className="wine-entry-price helper">{data.text.drink_info.price[lang]}</td>
+                                                </tr>
+                                                {/*  DRINK LIST */}
+                                                { this.makeDrinks('drinks', 'vermut', lang) }
+                                            </tbody>
+                                        </table>
+                                    </div>
+
+                                </div>
+                                <div className="col-6">
+                                    {/* Dessert wines */}
+                                    <h2 className="vermut-title">
+                                        {data.text.dessert_wines[lang]}
+                                        
+                                    </h2>
+
+                                    <div className="dessertwines-list">
+                                        <table className="dessertwines-table">
+                                            <tbody>
+                                                <tr className="other-drink-entry"> 
+                                                    {/* HELPERS */}
+                                                    <td className="wine-entry-name helper">{data.text.drink_info['blank']}</td>
+                                                    <td className="wine-entry-unit helper">{data.text.drink_info.size[lang]}</td>
+                                                    <td className="wine-entry-price helper">{data.text.drink_info.price[lang]}</td>
+                                                </tr>
+                                                {/*  DRINK LIST */}
+                                                { this.makeDrinks('drinks', 'dessert_wines', lang) }
+                                            </tbody>
+                                        </table>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+
+                    <div className="col-4">
+                        {/* Champagne */}
+                        <h2 className="champagne-title">{data.text.champagne[lang]}</h2>
+                        
+                                {/*  DRINK LIST */}
+                                { this.makeDrinksList('drinks', 'champagne', lang) }
+                            
+                    </div>
+                </div>
+
+
             </div>
         )
     }
